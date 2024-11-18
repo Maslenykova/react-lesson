0bc66e380680436bc22932d1f5e681251009ac9e
Maslenykova <smaslenykova8@gmail.com>
2024-11-18 17:44:28 +0200

    lesson3/task4

--- lesson3/task4/src/index.jsx
+++ lesson3/task4/src/index.jsx
@@ -4,11 +4,6 @@ import Greeting from "./Greeting";
 
 const rootElem = document.querySelector('#root');
 
-const userInfo = {
-    firstName: "John",
-    lastName: "Doe",
-    birthDate: "1985-06-19",
-};
 
 ReactDOM.render(
     <Greeting firstName="John" lastName="Doe" birthDate="2002-06-19" />,
