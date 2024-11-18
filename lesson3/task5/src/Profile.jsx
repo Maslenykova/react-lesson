5828e0da87f5be8313dc0e9a9072efa9c3ba4cf2
Maslenykova <smaslenykova8@gmail.com>
2024-11-18 19:31:55 +0200

    lesson3

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -7,7 +7,7 @@ function Profile(props) {
   const day = moment(props.birthDate).format('DD MMM YY');
   return (
     <div className="profile">
-     <div className='profile__name'>{`${props.user.firstName} ${props.user.lastName}`}</div>
+     <div className='profile__name'>{`${props.userData.firstName} ${props.userData.lastName}`}</div>
       <div className="profile__birth">{`Was born ${day} in London`}</div>
     </div>
   );
