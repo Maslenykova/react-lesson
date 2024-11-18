9dd32ac0c0578940878dffabf0d0b2cae90bc977
Maslenykova <smaslenykova8@gmail.com>
2024-11-18 17:21:47 +0200

    lesson3/task4

--- lesson3/task4/src/index.jsx
+++ lesson3/task4/src/index.jsx
@@ -4,8 +4,7 @@ import Greeting from "./Greeting";
 
 const rootElem = document.querySelector('#root');
 
-
 ReactDOM.render(
-    <Greeting firstName="John" lastName="Doe" birthDate="2006-11-14" />,
+    <Greeting firstName="John" lastName="Doe" birthDate="1985-06-19" />,
     rootElem
   );
