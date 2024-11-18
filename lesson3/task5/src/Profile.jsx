93227e98719f4f337480ce094b01cd7275c624b5
Maslenykova <smaslenykova8@gmail.com>
2024-11-18 19:58:40 +0200

    lesson3

--- lesson3/task5/src/Profile.jsx
+++ lesson3/task5/src/Profile.jsx
@@ -8,7 +8,7 @@ function Profile(props) {
   return (
     <div className="profile">
      <div className='profile__name'>{`${props.userData.firstName} ${props.userData.lastName}`}</div>
-      <div className="profile__birth">{`Was born ${day} in London`}</div>
+      <div className="profile__birth">{`Was born ${day} in ${props.userData.birthPlace}`}</div>
     </div>
   );
 }
