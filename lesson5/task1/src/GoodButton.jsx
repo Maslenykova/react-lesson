9b56a74a32f098f45ffb728a128a1a366c53dfd5
Maslenykova <smaslenykova8@gmail.com>
2024-11-20 11:52:50 +0200

    lesson5/task1

---
+++ lesson5/task1/src/GoodButton.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './index.scss';
+
+const GoodButton = () =>{
+    return(<button className="fancy-button"
+    onClick={() => alert('Good job!')}
+    >Click me!</button>)
+
+}
+
+export default GoodButton;
+
+
+
+
+
