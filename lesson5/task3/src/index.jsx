ae93dec74848ffd62a04124d0a5c58036002d641
Maslenykova <smaslenykova8@gmail.com>
2024-11-20 16:04:57 +0200

    lesson5/task3

---
+++ lesson5/task3/src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Counter from "./Counter";
+
+const rootElem = document.querySelector('#root');
+
+
+ReactDOM.render(
+    <Counter />,
+    rootElem
+  );
