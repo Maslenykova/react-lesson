2e110c0030b4d59219875d5795c8e3a0c8df779d
Maslenykova <smaslenykova8@gmail.com>
2024-11-20 17:35:44 +0200

    lesson5/task4

---
+++ lesson5/task4/src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Colors from "./Colors";
+
+const rootElem = document.querySelector('#root');
+
+
+ReactDOM.render(
+    <Colors />,
+    rootElem
+  );
