6c4b8f4fb69d64ea85988ddd5b9af2ca1c9c93be
Maslenykova <smaslenykova8@gmail.com>
2024-11-22 19:21:32 +0200

    lesson6

--- lesson6/task1/src/Auth.jsx
+++ lesson6/task1/src/Auth.jsx
@@ -1,46 +1,44 @@
 import React, { Component } from "react";
 import Greeting from './Greeting'; 
+import Login from "./Login";
+import Logout from "./Logout";
 import './index.scss';
 
 class Auth extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoggedIn: false,
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        isLoggedIn: false,
+      };
+    }
+  
+    handleLogin = () => {
+      this.setState({
+        isLoggedIn: true,
+      });
     };
+  
+    handleLogout = () => {
+      this.setState({
+        isLoggedIn: false,
+      });
+    };
+  
+    render() {
+      const { isLoggedIn } = this.state;
+  
+      return (
+        <div className="panel">
+          <Greeting isLoggedIn={isLoggedIn} />
+          {isLoggedIn ? (
+            <Logout onLogout={this.handleLogout} />
+          ) : (
+            <Login onLogin={this.handleLogin} />
+          )}
+        </div>
+      );
+    }
   }
-
-  handleLogin = () => {
-    this.setState({
-      isLoggedIn: true,
-    });
-  };
-
-  handleLogout = () => {
-    this.setState({
-      isLoggedIn: false,
-    });
-  };
-
-  render() {
-    return (
-      <div className="panel">
- 
-        <Greeting isLoggedIn={this.state.isLoggedIn} />
-
-        {this.state.isLoggedIn ? (
-          <button className="btn logout" onClick={this.handleLogout}>
-            Logout
-          </button>
-        ) : (
-          <button className="btn login" onClick={this.handleLogin}>
-            Login
-          </button>
-        )}
-      </div>
-    );
-  }
-}
-
-export default Auth;
+  
+  export default Auth;
