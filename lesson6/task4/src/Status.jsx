601d557b26601c7c3f5e1b1ee3c84dfce012b881
Maslenykova <smaslenykova8@gmail.com>
2024-11-24 19:05:14 +0200

    les

--- lesson6/task4/src/Status.jsx
+++ lesson6/task4/src/Status.jsx
@@ -8,15 +8,17 @@ class Status extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        isOnline: false || true , 
+        isOnline: false, 
       };
     }
   
     render() {
-   if(this.state.isOnline === true){
-           return (<Online/>)
-    }  
-   return (<Offline/>)  
+//    if(this.state.isOnline === true){
+//            return (<Online/>)
+//     }  
+//    return (<Offline/>) 
+   const { isOnline } = this.state; 
+   return isOnline ? <Online /> : <Offline />;
  }
   }
   
