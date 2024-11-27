b7e60717a6d3c91b4ab443e585b72b550b906a03
Maslenykova <smaslenykova8@gmail.com>
2024-11-27 15:53:11 +0200

    lesson3/task3

---
+++ lesson7/task3/src/Transaction.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import moment from 'moment';
+
+
+const Transaction = ({ from, to, amount, time, rate }) => {
+  const formatDate = time => moment(time).format('DD MMM YYYY');
+  const formatTime = time => moment(time).format('HH:mm');
+  const formattedAmount = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount);
+
+  return (
+    <li className="transaction">
+      <span className="transaction__date">{formatDate(time)}</span>
+      <span className="transaction__time">{formatTime(time)}</span>
+      <span className="transaction__assets">{from} → {to}</span>
+      <span className="transaction__rate">{rate}</span>
+      <span className="transaction__amount">{formattedAmount}</span>
+    </li>
+  );
+};
+
+export default Transaction;
