a31dd3c45ad174d305389f2a2a4f0a12d6da5a71
Maslenykova <smaslenykova8@gmail.com>
2024-11-27 17:04:16 +0200

    lesson8/task1

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -10,6 +10,7 @@ class Life extends Component{
     }
     shouldComponentUpdate(nextProps, nextState){
         console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render')
+        return true;
     }
     componentDidUpdate(prevProps, prevState){
         console.log('componentDidUpdate(prevProps, prevState): some updates based on new props')
