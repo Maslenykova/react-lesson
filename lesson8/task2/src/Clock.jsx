59cf3ea9479d965f56d6f134a10775909b6b0373
Maslenykova <smaslenykova8@gmail.com>
2024-11-28 17:02:26 +0200

    lesson8/task2

--- lesson8/task2/src/Clock.jsx
+++ lesson8/task2/src/Clock.jsx
@@ -1,41 +1,44 @@
 import React, {Component} from 'react';
 
-class Clock extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-          date: new Date(),
-        };
-      }
-      componentDidMount(){
-        this.interval = setInterval(() => {
-          this.setState({
-            date: new Date(),
-          });
-          
-        }, 1000);
-      }
-      componentWillUnmount(){
-        clearInterval(this.interval);
-      }
-    
-      getTimeWithOffset(offset) {
-        const currentTime = new Date();
-        const utcTime = currentTime.getTime() + currentTime.getTimezoneOffset() * 60000;
-        return new Date(utcTime + offset * 3600000);
-      }
-    
-      render() {
-        const { location, offset } = this.props;
-        const localTime = this.getTimeWithOffset(offset);
-    
-        return (
-          <div className="clock">
-            <div className="clock__location">{location}</div>
-            <div className="clock__time">{localTime.toLocaleTimeString()}</div>
-          </div>
-        );
-    }
-}
 
-export default Clock;
+class Clock extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        location: props.location, 
+        offset: props.offset,     
+        time: this.getTimeWithOffset(props.offset),
+      };
+    }
+  
+    getTimeWithOffset(offset) {
+      const currentTime = new Date();
+      const utcOffset = currentTime.getTimezoneOffset() / 60; 
+      const localTime = new Date(
+        currentTime.setHours(currentTime.getHours() + offset + utcOffset)
+      );
+      return localTime.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: true });
+    }
+  
+    componentDidMount() {
+      this.interval = setInterval(() => {
+        this.setState({
+          time: this.getTimeWithOffset(this.state.offset),
+        });
+      }, 1000);
+    }
+    componentWillUnmount() {
+      clearInterval(this.interval); 
+    }
+  
+    render() {
+      return (
+        <div className="clock">
+          <div className="clock__location">{this.state.location}</div>
+          <div className="clock__time">{this.state.time}</div>
+        </div>
+      );
+    }
+  }
+  
+  export default Clock;
