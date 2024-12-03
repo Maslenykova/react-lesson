52950838466f41b8574c8f511854c91597fa473b
Maslenykova <smaslenykova8@gmail.com>
2024-12-03 15:10:03 +0200

    les

--- lesson9/task4/src/UsersList.jsx
+++ lesson9/task4/src/UsersList.jsx
@@ -1,33 +1,32 @@
 import React, { Component } from 'react';
-import User from './User';
 import Filter from './Filter';
+import User from './User';
 
 class UsersList extends Component {
   state = {
     filterText: '', 
   };
 
-  handleFilterChange = (newFilterText) => {
-    this.setState({ filterText: newFilterText }); 
+  handleFilterChange = (filterText) => {
+    this.setState({ filterText }); 
   };
 
   render() {
-    const { users } = this.props;
-    const { filterText } = this.state;
-
-    const filteredUsers = users.filter((user) =>
+    const { users } = this.props; 
+    const { filterText } = this.state; 
+    const filteredUsers = users.filter(user =>
       user.name.toLowerCase().includes(filterText.toLowerCase())
-    );
+    ); 
 
     return (
       <div>
         <Filter
           filterText={filterText}
           count={filteredUsers.length}
-          onChange={this.handleFilterChange}
+          onChange={this.handleFilterChange} 
         />
         <ul className="users">
-          {filteredUsers.map((user) => (
+          {filteredUsers.map(user => (
             <User key={user.id} {...user} />
           ))}
         </ul>
