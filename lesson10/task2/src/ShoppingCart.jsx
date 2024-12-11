774442996ca701e840ee1b1dc6b5486f4323a1d3
Maslenykova <smaslenykova8@gmail.com>
2024-12-11 10:21:42 +0200

    les

--- lesson10/task2/src/ShoppingCart.jsx
+++ lesson10/task2/src/ShoppingCart.jsx
@@ -1,35 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CartTitle from './CartTitle';
 import ProductsList from './ProductsList';
 
+class ShoppingCart extends Component {
+  state = {
+    cartItems: [
+      {
+        id: '1',
+        name: 'iPhone 11',
+        price: 999,
+      },
+      {
+        id: '2',
+        name: 'iPad Pro',
+        price: 799,
+      },
+    ],
+  };
 
-class ShoppingCart extends Component{
-        state = {
-            cartItems: [{
-                id: '1',
-                name: 'iPhone 11',
-                price: 999,
-            },
-            {
-                id: '2',
-                name: 'iPad Pro',
-                price: 799,
-            },
-            ]
-        }
-        render(){
-            const { cartItems } = this.state;
-            const { userData } = this.props;
-            const count = cartItems.length;
-        
-            return (
-              <div className="column">
-                <CartTitle userName={userData.firstName} count={count} />
-                <ProductsList cartItems={cartItems} />
-              </div>
-        )
-    }
-   
+  render() {
+    const count = this.state.cartItems.length;
+    const { userName } = this.props;
+
+    return (
+      <div className="column">
+        <CartTitle userName={userName} count={count} />
+        <ProductsList cartItems={this.state.cartItems} />
+      </div>
+    );
+  }
 }
 
 export default ShoppingCart;
