102470ea8157c1bc89741b0cee09af0ef854b625
Maslenykova <smaslenykova8@gmail.com>
2024-12-13 13:13:32 +0200

    les

--- lesson11/task2/src/Dialog.jsx
+++ lesson11/task2/src/Dialog.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Dialog =({isOpen, children, title, onClose})=>{
-        if(!isOpen){
-            return null;
-        }
-        return(
-            <div class="dialog">
-            <div class="dialog__heading">
-              <h4 class="dialog__title">{title}</h4>
-              <button class="dialog__close-btn" onClick={onClose}>+</button>
-            </div>
-            <div class="dialog__content">{children}</div>
-                </div>
-        )
-    }
+const Dialog = ({ isOpen, children, title, onClose }) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <div className="dialog">
+      <div className="dialog__heading">
+        <h4 className="dialog__title">{title}</h4>
+        <button className="dialog__close-btn" onClick={onClose}>
+          +
+        </button>
+      </div>
+      <div className="dialog__content">{children}</div>
+    </div>
+  );
+};
 
 Dialog.propTypes = {
-    isOpen: PropTypes.bool,
-    children: PropTypes.element.isRequired,
-    title: PropTypes.string,
-    onClose: PropTypes.func.isRequired
-}
+  isOpen: PropTypes.bool,
+  children: PropTypes.element.isRequired,
+  title: PropTypes.string,
+  onClose: PropTypes.func.isRequired,
+};
+
 Dialog.defaultProps = {
-    isOpen: false,
-    title: ''
-}
+  isOpen: false,
+  title: '',
+};
+
 export default Dialog;
