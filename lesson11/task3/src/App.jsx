bf90045c3d38c77b34d52aba14478bf5306d5abd
Maslenykova <smaslenykova8@gmail.com>
2024-12-13 15:49:01 +0200

    lesson11

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
+import React, { Component} from 'react';
 import Expand from './Expand';
 
-const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleToggle = () => {
-    setIsOpen((prevState) => !prevState);
+class App extends Component {
+  state = {
+    isOpen: false,
   };
 
+  handleToggle = () => {
+    this.setState((prevState) => ({
+      isOpen: !prevState.isOpen,
+    }));
+  };
+
+ render(){
   return (
-    <div className="app">
-      <Expand
-        title="Click to Expand"
-        isOpen={isOpen}
-        onToggle={handleToggle}
-      >
-        <p>
-          This is the main content. It appears when the expand button is toggled.
-        </p>
-      </Expand>
+    <div className="expand__content">
+     <Expand title='Some title'
+       isOpen={this.state.isOpen}
+       onToggle={this.handleToggle}>
+     <p>
+        Hooks are a new addition in React 16.8. They let you use state and other React features
+        without writing a class.
+      </p>
+     </Expand>
+     
     </div>
   );
+ }
 };
 
 export default App;
