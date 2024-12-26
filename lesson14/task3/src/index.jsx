fa5c00c38833e224fc1adff968ff37efef597c15
Maslenykova <smaslenykova8@gmail.com>
2024-12-26 19:28:03 +0200

    lesson14/task3

---
+++ lesson14/task3/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Dimensions from './Dimensions';
+
+ReactDOM.render(
+    <Dimensions />,
+    document.getElementById('root')
+);
