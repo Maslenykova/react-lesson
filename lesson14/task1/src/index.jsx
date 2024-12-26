218e4ef796b25d972c60e57742d32589a050ac69
Maslenykova <smaslenykova8@gmail.com>
2024-12-26 18:56:38 +0200

    lesson14

--- lesson14/task1/src/index.jsx
+++ lesson14/task1/src/index.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import Counter from './Counter';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import Counter from "./Counter";
 
-ReactDOM.render(
-    <Counter />,
-    document.getElementById('root')
-);
+ReactDOM.render(<Counter />, document.getElementById("root"));
